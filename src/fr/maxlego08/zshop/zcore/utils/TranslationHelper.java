67577332999b099821f257d5fc92789038c46102
Maxlego08 <maxlego08@sfr.fr>
2025-06-25 15:26:08 +0200

    :memo: Fix

--- src/fr/maxlego08/zshop/zcore/utils/TranslationHelper.java
+++ src/fr/maxlego08/zshop/zcore/utils/TranslationHelper.java
@@ -1,24 +1,13 @@
 package fr.maxlego08.zshop.zcore.utils;
 
-import fr.maxlego08.zshop.zcore.utils.plugins.Plugins;
-import fr.maxlego08.ztranslator.api.Translator;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class TranslationHelper {
 
-    /**
-     * Allows to translate the item name, if the zTranslator plugin is active, then the translated name will be retrieved
-     *
-     * @param offlinePlayer
-     * @param itemStack
-     * @return item name
-     */
     protected String getItemName(OfflinePlayer offlinePlayer, ItemStack itemStack) {
 
         if (itemStack == null) {
@@ -29,37 +18,15 @@ public abstract class TranslationHelper {
             return itemStack.getItemMeta().getDisplayName();
         }
 
-        if (Bukkit.getPluginManager().isPluginEnabled(Plugins.ZTRANSLATOR.getName())) {
-
-            RegisteredServiceProvider<Translator> provider = Bukkit.getServer().getServicesManager()
-                    .getRegistration(Translator.class);
-            Translator translator = provider.getProvider();
-            return translator.translate(offlinePlayer, itemStack);
-        }
-
         String name = itemStack.serialize().get("type").toString().replace("_", " ").toLowerCase();
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
-    /**
-     * Allows to translate the item name, if the zTranslator plugin is active, then the translated name will be retrieved
-     *
-     * @param itemStack
-     * @return item name
-     */
     public String getItemName(ItemStack itemStack) {
         if (itemStack == null) return "";
 
         if (itemStack.hasItemMeta() && itemStack.getItemMeta().hasDisplayName()) {
             return convertOldHexString(itemStack.getItemMeta().getDisplayName());
-        }
-
-        if (Bukkit.getPluginManager().isPluginEnabled(Plugins.ZTRANSLATOR.getName())) {
-
-            RegisteredServiceProvider<Translator> provider = Bukkit.getServer().getServicesManager()
-                    .getRegistration(Translator.class);
-            Translator translator = provider.getProvider();
-            return translator.translate(itemStack);
         }
 
         String name = itemStack.serialize().get("type").toString().replace("_", " ").toLowerCase();
